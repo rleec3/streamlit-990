a06741e2c34bacb86946bc16fe3fc0e0573c3135
Riana <166577991+rleec3@users.noreply.github.com>
2024-04-22 05:50:30 +0000

    update 2

--- pages/Nonprofit_Search_Tool_2.py
+++ pages/Nonprofit_Search_Tool_2.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 import time
